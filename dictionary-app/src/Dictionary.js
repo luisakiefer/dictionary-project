b7f5a8971d761b285b499edbfe77c607e3827cd5
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-06-27 16:14:10 -0300

    updated some css and created Meaning Component

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios"; 
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -26,6 +28,7 @@ export default function Dictionary() {
         <input type="text" onChange={handleKeywordSubmit} />
         <input type="submit" value="search" className="search-button"/>
       </form>
+      <Results results={results} /> 
     </div>
   );
 }
